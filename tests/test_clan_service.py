0df2fae7a9defd598539a2e0bdb42c81b3ab6518
RaphaelDP <raphael.diezpecoste@gmail.com>
2026-07-06 19:33:11 +0200

    test: Remove redundant test_upsert_member (covered by MemberService tests)

--- tests/test_clan_service.py
+++ tests/test_clan_service.py
@@ -17,36 +17,6 @@ from app.database.models.member import Member
 from app.services.clan_service import ClanService
 
 
-def test_upsert_member(db_session, mock_clan_data):
-    """Test the _upsert_member method of ClanService."""
-
-    clan_service = ClanService(db_session)
-
-    member_data = mock_clan_data["memberList"][0]
-
-    new_member = clan_service._upsert_member(member_data)
-
-    db_session.flush()
-
-    assert new_member.tag == "#TEST_PLAYER1"
-    assert new_member.name == "Player 1"
-    assert new_member.role == "Leader"
-    assert new_member.trophies == 5000
-    assert new_member.donations == 100
-    assert new_member.last_seen is not None
-
-    updated_data = member_data.copy()
-    updated_data["trophies"] = 6767
-
-    updated_member = clan_service._upsert_member(updated_data)
-
-    assert updated_member.trophies == 6767
-    assert updated_member.tag == "#TEST_PLAYER1"
-
-    members = db_session.query(Member).all()
-
-    assert len(members) == 1
-    assert members[0].tag == "#TEST_PLAYER1"
 
 
 def test_sync_clan_members(db_session, mocker, mock_clan_data):
