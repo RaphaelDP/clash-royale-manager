895c1a65e7613c86f089516f5f5122aa1baaa279
RaphaelDP <raphael.diezpecoste@gmail.com>
2026-09-16 09:50:00 +0200

    chore(services): add package docstring to __init__.py

---
+++ app/services/__init__.py
@@ -0,0 +1,10 @@
+"""
+================================================================================
+Filename: __init__.py
+Description: Empty file to make services a Python package.
+Version: 0.9.1
+Python Version: 3.12
+Dependencies: None
+================================================================================
+
+"""
